f0495f7a326668bb64cb2e391e3db361c6818eab
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-03-19 09:39:11 -0400

    not finished

---
+++ Unit2-01/MrCoxallStack.ts
@@ -0,0 +1,26 @@
+/**
+ * This is the program for using the MrCoxallStack class
+ *
+ * author Flynn Rundquist
+ * version 1.0
+ * since 2024-03-19
+ */
+
+class MrCoxallStack {
+	fruit_stack = MrCoxallStack()
+	fruit_stack.push('apple')
+	fruit_stack.push('banana')
+	fruit_stack.push('cherry')
+
+	colour_stack = MrCoxallStack()
+	colour_stack.push('red')
+	colour_stack.push('green')
+	colour_stack.push('blue')
+
+	mammal_stack = MrCoxallStack()
+	mammal_stack.push('dog')
+	mammanl_stack.push('cat')
+	mammal_stack('whale')
+}
+export = MrCoxallStack
+
---
+++ Unit2-01/index.ts
@@ -0,0 +1,10 @@
+/**
+ * This is the standard index.ts starting point.
+ *
+ * author Flynn Rundquist
+ * version 1.0
+ * since 2024-03-19
+ */
+import MyMrCoxallStack from './MrCoxallStack'
+
+
