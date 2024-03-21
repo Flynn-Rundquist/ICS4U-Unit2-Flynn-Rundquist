f9dfd1e5165f11153e0aeb1d9bee9e5dc2b99d3f
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-03-21 09:12:31 -0400

    fixing stuff

--- Unit2-01/FlynnStack.ts
+++ Unit2-01/FlynnStack.ts
@@ -7,25 +7,28 @@
  */
 
 class FlynnStack {
-    strstack: string[]
+    strStack: string[]
     
     constructor() {
-        this.strstack = []
+        this.strStack = []
     }
 
+    pop(): string {
+        return this.strStack.pop()
+    }
     push(input: string): void {
-        this.strstack.push(input)
+        this.strStack.push(input)
     }
 
     show(): string {
         let values = ""
-        for (let counter = 0; counter < this.strstack.length; counter++) {
-                values += this.strstack[counter] + ", "
+        for (let counter = 0; counter < this.strStack.length; counter++) {
+                values += this.strStack[counter] + ", "
         }
         values = values.substring(0, values.length - 2)
         return values
     }
 }
 
-export = FlynnStack
+export default = FlynnStack
 
