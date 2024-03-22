30cb9514678a733bbafb3d12eec916d869069757
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-03-22 09:36:47 -0400

    Update Main.java

--- Unit2-01/Main.java
+++ Unit2-01/Main.java
@@ -5,26 +5,21 @@
  * @version 1.0
  * @since 2024-03-22
  */
-final class Main {
-    /**
-     * This is the standard Main() class starting point.
-     *
-     * @param args Unused.
-     */
-    public static void main(final String[] args) {
-        FlynnStack<Stack> fruits = new FlynnStack<>();
+public class Main {
+    public static void main(String[] args) {
+        FlynnStack fruits = new FlynnStack(3);
         fruits.push("apple");
         fruits.push("banana");
         fruits.push("cherry");
         System.out.println(fruits.show());
 
-        FlynnStack<Stack> colours = new FlynnStack<>();
+        FlynnStack colours = new FlynnStack(3);
         colours.push("red");
         colours.push("blue");
         colours.push("purple");
         System.out.println(colours.show());
 
-        FlynnStack<Stack> animals = new FlynnStack<>();
+        FlynnStack animals = new FlynnStack(3);
         animals.push("dog");
         animals.push("bird");
         animals.push("cat");
@@ -33,4 +28,3 @@ final class Main {
         System.out.println("\nDone.");
     }
 }
-
