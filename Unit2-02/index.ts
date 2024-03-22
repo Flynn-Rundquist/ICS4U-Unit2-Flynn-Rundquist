5676cb5c2682e7900b98208aeb311367967f9c26
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-03-22 08:51:21 -0400

    Update index.ts

--- Unit2-02/index.ts
+++ Unit2-02/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+/**
+ * This is the standard index.ts starting point.
+ *
+ * author Flynn Rundquist
+ * version 1.0
+ * since 2024-03-19
+ */
+
+import FlynnStack from "./FlynnStack"
+
+let fruits = new FlynnStack()
+fruits.push("apple")
+fruits.push("banana")
+fruits.push("cherry")
+console.log("fruits: " + fruits.show())
+
+let colours = new FlynnStack()
+colours.push("red")
+colours.push("blue")
+colours.push("purple")
+console.log("colours: " + colours.show())
+
+let animals = new FlynnStack()
+animals.push("dog")
+animals.push("bird")
+animals.push("cat")
+console.log("animals: " + animals.show())
+
+console.log("\nDone.")
