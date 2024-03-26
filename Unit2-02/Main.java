ece5e519a4951b265b878599972838bf67dd6c33
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-03-26 09:39:01 -0400

    Update Main.java

--- Unit2-02/Main.java
+++ Unit2-02/Main.java
@@ -1,2 +1,36 @@
 /**
+ * This is the standard Main() class starting point.
  *
+ * @author Flynn Rundquist
+ * @version 1.0
+ * @since 2024-03-22
+ */
+
+public class Main {
+    /**
+     * The main() method.
+     *
+     * @param args The command line arguments
+     */
+    public static void main(String[] args) {
+        FlynnStack fruits = new FlynnStack();
+        fruits.push("apple");
+        fruits.push("banana");
+        fruits.push("cherry");
+        System.out.println("Fruits: " + fruits.show());
+
+        FlynnStack colours = new FlynnStack();
+        colours.push("red");
+        colours.push("blue");
+        colours.push("purple");
+        System.out.println("Colours: " + colours.show());
+
+        FlynnStack animals = new FlynnStack();
+        animals.push("dog");
+        animals.push("bird");
+        animals.push("cat");
+        System.out.println("Animals: " + animals.show());
+
+        System.out.println("\nDone.");
+    }
+}
