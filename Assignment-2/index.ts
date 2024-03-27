84e748f669478ea85ba03f4479adf29f1b86db5b
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-03-27 18:19:49 -0400

    starting the assignment

--- Assignment-2/index.ts
+++ Assignment-2/index.ts
@@ -4,35 +4,4 @@
  *
  * author Flynn Rundquist
  * version 1.0
- * since 2024-04-03
- */
-
-import { createPrompt } from "bun-promptx"
-
-    let maxRun = 0
-    let currentRun = 0
-    let previousChar = ""
-    let char = ""
-
-    // Get input
-    const strInput = createPrompt("Enter a string: ")
-
-    // Split string into array
-    const arrInput = strInput[0] = strInput.split("")
-
-for (let counter = 0; counter < arrInput.length; counter++) {
-    char = arrInput[counter]
-    if (char === previousChar) {
-        currentRun += 1
-        if (currentRun > maxRun) {
-            maxRun = currentRun
-        }
-    } else {
-        currentRun = 0
-    }
-    previousChar = char
-}
-
-console.log(`The largest run of zeroes is ${maxRun}.`)
-console.log("\nDone.")
-
+ * since 2024-04
