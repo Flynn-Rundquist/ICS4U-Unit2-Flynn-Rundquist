4fe2c58b3d562bf683589f2f0295e0e26bf896fe
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-04-12 09:01:42 -0400

    Update Bike.ts

--- Unit2-07/Bike.ts
+++ Unit2-07/Bike.ts
@@ -6,37 +6,30 @@
  * Since: 2022-04-10
  */
 
-import Vehicle from './Vehicle'
+import Vehicle from './Vehicle.ts'
 
 class Bike extends Vehicle {
-    public cadense: number
-    constructor(colour = 'unknown', maxSpeed = 0, cadense = 0, speed = 0) {
-        super()
-        this.cadense = cadense
-        this.color = colour
-        this.maxSpeed = maxSpeed
-        this.speed = speed
-    }
-    // accelerate
-    accelerate(appliedPower: number) {
-        this.cadense += appliedPower
-        return this.cadense
-    }
+  private cadence: number
 
-    // ring bell
-    ringBell() {
-        console.log('Ring ring!')
-    }
+  constructor(color = 'unknown', maxSpeed = 0, speed = 0, cadence = 0) {
+    super(color, maxSpeed, speed)
+    this.cadence = cadence
+  }
 
-    // status
-    status() {
-        this.speed = this.cadense * 2
-        console.log(`-> Speed: ${this.speed}`)
-        console.log(`-> Max Speed: ${this.maxSpeed}`)
-        console.log(`-> Color: ${this.color}`)
-        console.log(`-> Cadence: ${this.cadense}`)
-    }
+  // accelerate
+  accelerate(appliedPower: number) {
+    this.cadence += appliedPower
+    return this.cadence
+  }
+
+  // ring bell
+  Ring_Bell() {
+    console.log('Ring ring!')
+  }
+
+  cadencestatus (): void {
+    console.log(`-> Cadence: ${this.cadence}`)
+  }
 }
 
 export default Bike
-
