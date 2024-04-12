ca687bdd141a722c57f88ce4c69513f1ac40e06e
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-04-12 08:45:16 -0400

    Update index.ts

--- Unit2-07/index.ts
+++ Unit2-07/index.ts
@@ -6,30 +6,35 @@
 * @since   2022-04-10
 */
 
-import Truck from "./Truck"
-import Bike from "./Bike"
+import Truck from "./Truck.ts"
+import Bike from "./Bike.ts"
 
-const bmx = new Bike('Red', 40)
+const bmx = new Bike(0, 40, 'Red', 0)
 
 console.log('Created Bmx bike.\nStatus:\n')
 bmx.status()
+bmx.cadencestatus()
 
 console.log('Set the cadense to 10\n')
-bmx.cadense = 10
+bmx.speed = 20
+bmx.cadence = 10
 bmx.status()
+bmx.cadencestatus()
 
 console.log('\nAccelerate by 15:')
 bmx.accelerate(15)
+bmx.speed = 50
+bmx.cadence = 25
 bmx.status()
+bmx.cadencestatus()
 
 console.log('\nRing bell.')
-bmx.ringBell()
 
-const bigTruck = new Truck('Grey', 200, 'HGC-3456F')
+const bigTruck = new Truck(0, 200, 'Grey', 'HGC-3456F')
 
-console.log('\nCreated a Truck.\nStatus:\n')
+console.log('Created a Truck.\nStatus:\n')
 bigTruck.status()
-
+bigTruck.plateStatus()
 console.log('\nAccelerating, 10 of power for ten seconds:')
 bigTruck.accelerate(10, 10)
 console.log('New speed: ' + bigTruck.speed)
@@ -39,7 +44,7 @@ bigTruck.accelerate(10, 10)
 console.log('New speed: ' + bigTruck.speed)
 
 console.log('\nApplyed air pressure of 10:')
-bigTruck.applyAir(10)
+bigTruck.applyAir(390, 390)
 console.log('New speed: ' + bigTruck.speed)
 
 console.log('\nDone.')
