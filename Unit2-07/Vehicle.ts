7161ff5def157300508aa0f6367c518c2d9cf245
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-04-12 08:59:53 -0400

    Update Vehicle.ts

--- Unit2-07/Vehicle.ts
+++ Unit2-07/Vehicle.ts
@@ -6,33 +6,32 @@
  * Since:   2024-04-02
  */
 class Vehicle {
-    protected color: string
-    protected speed: number
-    protected maxSpeed: number
+  protected color: string
+  protected speed: number
+  protected maxSpeed: number
 
-    constructor (color = 'unknown', maxSpeed = 0, speed = 0) {
-        this.color = color
-        this.speed = speed
-        this.maxSpeed = maxSpeed
-    }
-    // acceleration method
-    accelerate (accelerationPower: number, accelerationTime: number) {
-        this.speed += (accelerationPower * accelerationTime)
-        return this.speed
-    }
+  constructor (speed = 0, maxSpeed = 0, color = 'unknown') {
+    this.color = color
+    this.speed = speed
+    this.maxSpeed = maxSpeed
+  }
+  // acceleration method
+  accelerate (accelerationPower: number, accelerationTime: number) {
+    this.speed += (accelerationPower * accelerationTime)
+    return this.speed
+  }
 
-    // brake method
-    break (brakePower: number, brakeTime: number) {
-        this.speed -= (brakePower * brakeTime)
-        return this.speed
-    }
+  // brake method
+  break (brakePower: number, brakeTime: number) {
+    this.speed -= (brakePower * brakeTime)
+    return this.speed
+  }
 
-    // status method
-    status (): void {
-        console.log(`-> Speed: ${this.speed}`)
-        console.log(`-> Max Speed: ${this.maxSpeed}`)
-        console.log(`-> Color: ${this.color}`)
-    }
+  // status method
+  status (): void {
+    console.log(`-> Speed: ${this.speed}`)
+    console.log(`-> Max Speed: ${this.maxSpeed}`)
+    console.log(`-> Color: ${this.color}`)
+  }
 }
 export default Vehicle
-
