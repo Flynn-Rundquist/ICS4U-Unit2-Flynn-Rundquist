cbf9925a343ea43874cafcf6f3ec0bc066726354
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-04-17 19:08:20 -0400

    still linting ToT

--- Assignment-2/Main.java
+++ Assignment-2/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * This program returns the longest run in a string.
  *
@@ -6,7 +8,6 @@
  * @since 2024-04-17
  */
 
-import java.util.Scanner;
 
 final class Main {
     /**
