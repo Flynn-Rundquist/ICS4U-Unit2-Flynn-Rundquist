c3e50e8a75e95e4140fab269bcba8fb4c48a9d44
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-04-17 18:07:03 -0400

    Update index.ts

--- Assignment-2/index.ts
+++ Assignment-2/index.ts
@@ -7,29 +7,40 @@
  * since 2024-04
  */
 
-import * as readline from 'readline'
+import { createPrompt } from "bun-promptx"
 
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-reader.question("Enter a string: ", (userInput) => {
-  const userArray = userInput.split("")
-  let maxRun = 0
-
-  for(let counter = 0; counter < userArray.length; counter++) {
-    let currentRun = 1
-    while (userArray[counter] === userArray[counter + currentRun]) {
-      currentRun++
+function blowup(inputString: string): string {
+  let result = ''
+  let index = 0
+  while (index < inputString.length) {
+    if (/\d/.test(inputString[index])) {
+      if (index + 1 < inputString.length && /[a-zA-Z]/.test(inputString[index + 1])) {
+        const num = Number(inputString[index])
+        const charToRepeat = inputString[index + 1]
+        result += charToRepeat.repeat(num)
+        index++
+      }
+    } else {
+      result += inputString[index]
     }
-    if (currentRun > maxRun) {
-      maxRun = currentRun
-    }
+    index++
   }
+  return result
+}
 
-  console.log(`The largest run in the string is ${maxRun}.`)
-  console.log(`\nDone.`)
-  reader.close()
-})
-
+// user input
+const userInput = createPrompt("Enter a string: ")
+const inputStr = userInput.value
+if (inputStr !== null) {
+  if (inputStr.length === 1 && /[a-zA-Z]/.test(inputStr)) {
+    console.log("Enter a string with more than one character.")
+  } else if (inputStr.length === 1 && /\d/.test(inputStr)) {
+    console.log("Enter a string with more than one character.")
+  } else {
+    const output = blowup(inputStr)
+    console.log(`Result: ${output}`)
+  }
+} else {
+  console.log("Invalid input.");
+}
+console.log("\nDone.")
