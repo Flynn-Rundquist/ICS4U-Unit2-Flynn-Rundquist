2ae0104940c66bc246b843e7759f2815d258dbf8
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-04-17 18:20:49 -0400

    fixing code

--- Assignment-2/index.ts
+++ Assignment-2/index.ts
@@ -1,6 +1,6 @@
 /**
- * This program returns the largest run of zeroes of more adjacent characters
- * that are the same (ex. xxyyyz -> 3).
+ * This program returns the largest run of adjacent characters that are the same.
+ * (ex. abbb has a run of 3).
  *
  * author Flynn Rundquist
  * version 1.0
@@ -9,38 +9,44 @@
 
 import { createPrompt } from "bun-promptx"
 
-function blowup(inputString: string): string {
-  let result = ''
-  let index = 0
-  while (index < inputString.length) {
-    if (/\d/.test(inputString[index])) {
-      if (index + 1 < inputString.length && /[a-zA-Z]/.test(inputString[index + 1])) {
-        const num = Number(inputString[index])
-        const charToRepeat = inputString[index + 1]
-        result += charToRepeat.repeat(num)
-        index++
-      }
+function longestRun(inputString: string): number {
+  let currentRun = 1 // Length of the current run
+  let longestRun = 1 // Length of the longest run found so far
+  let counter = 1 // Counter to iterate through the string
+
+  while (counter < inputString.length) {
+    if (inputString[counter] === inputString[counter - 1]) {
+      // Increment the current run length if the current character matches the previous one
+      currentRun++
     } else {
-      result += inputString[index]
+      // Reset the current run length if the current character doesn't match the previous one
+      currentRun = 1
     }
-    index++
+
+    // Update the longest run if the current run is longer
+    if (currentRun > longestRun) {
+      longestRun = currentRun
+    }
+
+    counter++
   }
-  return result
+
+  return longestRun
 }
 
-// user input
+// User input
 const userInput = createPrompt("Enter a string: ")
 const inputStr = userInput.value
+
 if (inputStr !== null) {
-  if (inputStr.length === 1 && /[a-zA-Z]/.test(inputStr)) {
-    console.log("Enter a string with more than one character.")
-  } else if (inputStr.length === 1 && /\d/.test(inputStr)) {
+  if (inputStr.length === 1) {
     console.log("Enter a string with more than one character.")
   } else {
-    const output = blowup(inputStr)
-    console.log(`Result: ${output}`)
+    const output = longestRun(inputStr)
+    console.log(`Longest run: ${output}`)
   }
 } else {
-  console.log("Invalid input.");
+  console.log("Invalid input.")
 }
+
 console.log("\nDone.")
