46d803cee984528987a73c0a866241ad152b8fb8
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-04-17 18:24:58 -0400

    ts code working

--- Assignment-2/index.ts
+++ Assignment-2/index.ts
@@ -9,21 +9,18 @@
 
 import { createPrompt } from "bun-promptx"
 
-function longestRun(inputString: string): number {
-  let currentRun = 1 // Length of the current run
-  let longestRun = 1 // Length of the longest run found so far
-  let counter = 1 // Counter to iterate through the string
+function longestRun(inputString: string) {
+  let currentRun = 1
+  let longestRun = 1
+  let counter = 1
 
   while (counter < inputString.length) {
     if (inputString[counter] === inputString[counter - 1]) {
-      // Increment the current run length if the current character matches the previous one
       currentRun++
     } else {
-      // Reset the current run length if the current character doesn't match the previous one
       currentRun = 1
     }
 
-    // Update the longest run if the current run is longer
     if (currentRun > longestRun) {
       longestRun = currentRun
     }
