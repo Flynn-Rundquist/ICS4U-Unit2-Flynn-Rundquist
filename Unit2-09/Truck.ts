82a87e16a564981bd4e42010e0a7a31eb23d8a40
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-04-17 08:44:23 -0400

    fixing truck breaks

--- Unit2-09/Truck.ts
+++ Unit2-09/Truck.ts
@@ -20,7 +20,7 @@ class Truck extends Vehicle {
 
   // air breaks
   public break (breakPower: number, breakTime: number, airPressure?: number): void {
-    this.speed -= (breakPower * breakTime) - (airPressure ?? 0 * breakTime)
+    this.speed -= (breakPower * breakTime) - ((airPressure ?? 0) * breakTime)
     console.log(`New Speed: ${this.speed}`)
   }
 
