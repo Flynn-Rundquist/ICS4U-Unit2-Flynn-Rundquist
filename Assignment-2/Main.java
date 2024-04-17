0b4eb322364758ff05253fca0daaace05a5096fa
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-04-17 18:44:24 -0400

    linting

--- Assignment-2/Main.java
+++ Assignment-2/Main.java
@@ -15,16 +15,25 @@ public class Main {
      * @param args The command-line arguments
      */
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
+private Main() {
+        // Private constructor to prevent instantiation
+    }
+
+    /**
+     * The main method to execute the program.
+     * 
+     * @param args The command-line arguments
+     */
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a string: ");
-        String inputStr = scanner.nextLine();
-        
+        final String inputStr = scanner.nextLine();
+
         if (inputStr.length() == 1) {
             System.out.println("Enter a string with more than one character.");
         } else {
-            int output = longestRun(inputStr);
+            final int output = longestRun(inputStr);
             System.out.println("Longest run: " + output);
         }
 
@@ -32,7 +41,13 @@ public class Main {
         scanner.close();
     }
 
-    public static int longestRun(String inputString) {
+    /**
+     * Calculates the longest run of adjacent characters in a string.
+     * 
+     * @param inputString The input string
+     * @return The length of the longest run
+     */
+    public static int longestRun(final String inputString) {
         int currentRun = 1;
         int longestRun = 1;
         int counter = 1;
@@ -54,3 +69,4 @@ public class Main {
         return longestRun;
     }
 }
+
