27094cfe52e54cf39fcdd4d5c970ac6ba51ef14a
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-04-22 09:38:58 -0400

    ts assignment 3 working

---
+++ Assignment-3/Triangle.ts
@@ -0,0 +1,98 @@
+/**
+ * This is the Triangle() class.
+ * 
+ * By:      Flynn Rundquist
+ * Version: 1.0
+ * Since:   2024-04-23
+ */
+
+class Triangle {
+    private side1: number
+    private side2: number
+    private side3: number
+    
+    constructor (side1: number, side2: number, side3: number) {
+        this.side1 = side1
+        this.side2 = side2
+        this.side3 = side3
+    }
+
+    // is valid method
+    public isValid (): boolean {
+        if (this.side1 + this.side2 > this.side3 && this.side2 + this.side3 > this.side1 && this.side3 + this.side1 > this.side2) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    // semiPerimeter method
+    public semiPerimeter (): number {
+        let semiPerimeter = (this.side1 + this.side2 + this.side3) / 2
+        return semiPerimeter
+    }
+    // area method
+    public area (): number {
+        let semiPerimeter = this.semiPerimeter()
+        let area = Math.sqrt(semiPerimeter * (semiPerimeter - this.side1) * (semiPerimeter - this.side2) * (semiPerimeter - this.side3))
+        return area
+    }
+    // get type method
+    public getType (): string {
+        if (this.side1 == this.side2 && this.side2 == this.side3) {
+            return 'Equilateral'
+        } else if (this.side1 == this.side2 || this.side2 == this.side3 || this.side3 == this.side1) {
+            return 'Isosceles'
+        } else if (Math.sqrt(this.side1) + Math.sqrt(this.side2) == Math.sqrt(this.side3) || Math.sqrt(this.side2) + Math.sqrt(this.side3) == Math.sqrt(this.side1) || Math.sqrt(this.side3) + Math.sqrt(this.side1) == Math.sqrt(this.side2)) {
+            return 'Right Angle'
+        } else {
+            return 'Scalene'
+        }
+    }
+
+    // angle method in rads
+    public angle (angleNumber: number) {
+        if (angleNumber == 1) {
+            let angle = Math.acos((Math.pow(this.side2, 2) + Math.pow(this.side3, 2) - Math.pow(this.side1, 2)) / (2 * this.side2 * this.side3))
+            return angle
+        } else if (angleNumber == 2) {
+            let angle = Math.acos((Math.pow(this.side1, 2) + Math.pow(this.side3, 2) - Math.pow(this.side2, 2)) / (2 * this.side1 * this.side3))
+            return angle
+        } else if (angleNumber == 3) {
+            let angle = Math.acos((Math.pow(this.side1, 2) + Math.pow(this.side2, 2) - Math.pow(this.side3, 2)) / (2 * this.side1 * this.side2))
+            return angle
+        }
+    }
+    
+    // get height method
+    public height (sideNumber: number): number {
+        if (sideNumber == 1) {
+            let height = (2 * this.area()) / this.side1
+            return height
+        } else if (sideNumber == 2) {
+            let height = (2 * this.area()) / this.side2
+            return height
+        } else if (sideNumber == 3) {
+            let height = (2 * this.area()) / this.side3
+            return height
+        }
+    }
+
+    // inner circle radius method
+    public innerCircleRadius (): number {
+        let area = this.area()
+        let semiPerimeter = this.semiPerimeter()
+        let innerCircleRadius = area / semiPerimeter
+        return innerCircleRadius
+    }
+
+    // circumference of inner circle method
+    public innerCircleCircumference (): number {
+        let innerCircleRadius = this.innerCircleRadius()
+        let innerCircleCircumference = 2 * Math.PI * innerCircleRadius
+        return innerCircleCircumference
+    }
+    
+}
+
+export default Triangle
---
+++ Assignment-3/index.ts
@@ -0,0 +1,58 @@
+/**
+ * This is the code that runs with the Triangle class.
+ * 
+ * By:      Flynn Rundquist
+ * Version: 1.0
+ * Since:   2024-04-23
+ */
+
+import Triangle from './Triangle'
+
+const triangle1 = new Triangle(3, 3, 3)
+console.log('Created a 3 mm by 3 mm by 3 mm triangle.')
+console.log('Is it valid? ' + triangle1.isValid())
+console.log(`Semiperimeter: ${triangle1.semiPerimeter().toFixed(4)} mm`)
+console.log(`Area: ${triangle1.area().toFixed(4)} mm^2`)
+console.log(`Type: ${triangle1.getType()} triangle`)
+console.log(`Angle 1: ${triangle1.angle(1).toFixed(4)} radians`)
+console.log(`Angle 2: ${triangle1.angle(2).toFixed(4)} radians`)
+console.log(`Angle 3: ${triangle1.angle(3).toFixed(4)} radians`)
+console.log(`Height 1: ${triangle1.height(1).toFixed(4)} mm`)
+console.log(`Height 2: ${triangle1.height(2).toFixed(4)} mm`)
+console.log(`Height 3: ${triangle1.height(3).toFixed(4)} mm`)
+console.log(`Inner circle radius: ${triangle1.innerCircleRadius().toFixed(4)} mm`)
+console.log(`Inner circle circumference: ${triangle1.innerCircleCircumference().toFixed(4)} mm`)
+console.log("\n")
+
+const triangle2 = new Triangle(3, 4, 5)
+console.log('Created a 3 mm by 4 mm by 5 mm triangle.')
+console.log('Is it valid? ' + triangle2.isValid())
+console.log(`Semiperimeter: ${triangle2.semiPerimeter().toFixed(4)} mm`)
+console.log(`Area: ${triangle2.area().toFixed(4)} mm^2`)
+console.log(`Type: ${triangle2.getType()} triangle`)
+console.log(`Angle 1: ${triangle2.angle(1).toFixed(4)} radians`)
+console.log(`Angle 2: ${triangle2.angle(2).toFixed(4)} radians`)
+console.log(`Angle 3: ${triangle2.angle(3).toFixed(4)} radians`)
+console.log(`Height 1: ${triangle2.height(1).toFixed(4)} mm`)
+console.log(`Height 2: ${triangle2.height(2).toFixed(4)} mm`)
+console.log(`Height 3: ${triangle2.height(3).toFixed(4)} mm`)
+console.log(`Inner circle radius: ${triangle2.innerCircleRadius().toFixed(4)} mm`)
+console.log(`Inner circle circumference: ${triangle2.innerCircleCircumference().toFixed(4)} mm`)
+console.log("\n")
+
+const triangle3 = new Triangle(3, 4, 4)
+console.log('Created a 3 mm by 4 mm by 4 mm triangle.')
+console.log('Is it valid? ' + triangle3.isValid())
+console.log(`Semiperimeter: ${triangle3.semiPerimeter().toFixed(4)} mm`)
+console.log(`Area: ${triangle3.area().toFixed(4)} mm^2`)
+console.log(`Type: ${triangle3.getType()} triangle`)
+console.log(`Angle 1: ${triangle3.angle(1).toFixed(4)} radians`)
+console.log(`Angle 2: ${triangle3.angle(2).toFixed(4)} radians`)
+console.log(`Angle 3: ${triangle3.angle(3).toFixed(4)} radians`)
+console.log(`Height 1: ${triangle3.height(1).toFixed(4)} mm`)
+console.log(`Height 2: ${triangle3.height(2).toFixed(4)} mm`)
+console.log(`Height 3: ${triangle3.height(3).toFixed(4)} mm`)
+console.log(`Inner circle radius: ${triangle3.innerCircleRadius().toFixed(4)} mm`)
+console.log(`Inner circle circumference: ${triangle3.innerCircleCircumference().toFixed(4)} mm`)
+
+console.log("\nDone.")
