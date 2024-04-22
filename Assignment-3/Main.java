cc54bf928debe061697667158968dc506663c8ab
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-04-22 16:48:13 -0400

    starting java

---
+++ Assignment-3/Main.java
@@ -0,0 +1,5 @@
+package Unit2.ICS4U-Unit2-Flynn-Rundquist.Assignment-3;
+
+public class Main {
+    
+}
---
+++ Assignment-3/Triangle.java
@@ -0,0 +1,25 @@
+/**
+ * The triangle class.
+ *
+ * @author Flynn Rundquist
+ * @version 1.0
+ * @since 2021-04-22
+ */
+
+
+public class Triangle {
+    private int side1;
+    private int side2;
+    private int side3;
+    
+    public Triangle() {
+        side1 = 0;
+        side2 = 0;
+        side3 = 0;
+    }
+
+    public double semiPerimeter() {
+        const semiPerimeter = 
+    }
+
+}
