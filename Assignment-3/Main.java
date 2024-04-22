d5c6c9cd6bc6e4a5b172cc932d29344cef33fc70
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-04-22 17:12:24 -0400

    java not finished

--- Assignment-3/Main.java
+++ Assignment-3/Main.java
@@ -1,5 +1,30 @@
-package Unit2.ICS4U-Unit2-Flynn-Rundquist.Assignment-3;
+/**
+ * This is the Main() starting point.
+ * 
+ * @author Flynn Rundquist
+ * @version 1.0
+ * @since 2024-04-23
+ */
 
-public class Main {
-    
+final class Main {
+    /**
+     * The main() method.
+     *
+     * @param args The command line arguments
+     */
+    public static void main(String[] args) {
+
+        Triangle triangle1 = new Triangle(3, 4, 5);
+        System.out.println("Created a 3 mm by 4 mm by 5 mm triangle.");
+        System.out.println("Is the triangle valid? " + triangle1.isValid());
+        System.out.println("The semi-perimeter of the triangle is: " + triangle1.semiPerimeter());
+        System.out.println("The area of the triangle is: " + triangle1.area());
+        System.out.println("The type of the triangle is: " + triangle1.getType());
+        System.out.println("The first angle of the triangle is: " + triangle1.angle(1));
+        System.out.println("The second angle of the triangle is: " + triangle1.angle(2));
+        System.out.println("The third angle of the triangle is: " + triangle1.angle(3));
+
+        Triangle triangle2 = new Triangle(3, 3, 3);
+
+    }
 }
--- Assignment-3/Triangle.java
+++ Assignment-3/Triangle.java
@@ -3,7 +3,7 @@
  *
  * @author Flynn Rundquist
  * @version 1.0
- * @since 2021-04-22
+ * @since 2024-04-22
  */
 
 
@@ -18,8 +18,89 @@ public class Triangle {
         side3 = 0;
     }
 
+    public String isValid() {
+        if (side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side1) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+
     public double semiPerimeter() {
-        const semiPerimeter = 
+        int semiPerimeter = (side1 + side2 + side3) / 2;
+        return semiPerimeter;
+    }
+
+    public double area() {
+        if (!isValid().equals("false")) {
+            return -1;
+        } else {
+            double area = Math.sqrt(semiPerimeter() * (semiPerimeter() - side1) * (semiPerimeter() - side2) * (semiPerimeter() - side3));
+            return area;
+        }
+    }
+
+    public String getType() {
+        if (side1 == side2 && side2 == side3) {
+            return "Equilateral";
+        } else if (side1 == side2 || side2 == side3 || side3 == side1) {
+            return "Isosceles";
+        } else {
+            return "Scalene";
+        }
+    }
+
+    public double angle(int angleNumber) {
+        if (!isValid().equals("false")) {
+            return -1;
+        }
+
+        if (angleNumber == 1) {
+            double angle = Math.acos((Math.pow(side2, 2) + Math.pow(side3, 2) - Math.pow(side1, 2)) / (2 * side2 * side3));
+            return angle;
+        } else if (angleNumber == 2) {
+            double angle = Math.acos((Math.pow(side3, 2) + Math.pow(side1, 2) - Math.pow(side2, 2)) / (2 * side3 * side1));
+            return angle;
+        } else if (angleNumber == 3) {
+            double angle = Math.acos((Math.pow(side1, 2) + Math.pow(side2, 2) - Math.pow(side3, 2)) / (2 * side1 * side2));
+            return angle;
+        }
+    }
+
+    public double height(int sideNumber) {
+        if (!isValid().equals("false")) {
+            return -1;
+        }
+
+        if (sideNumber == 1) {
+            double height = (2 * area() / side1);
+            return height;
+        } else if (sideNumber == 2) {
+            double height = (2 * area() / side2);
+            return height;
+        } else if (sideNumber == 3) {
+            double height = (2 * area() / side3);
+            return height;
+        }
+        }
+
+        public double innerCircleRadius() {
+            if (!isValid.equals("false")) {
+                return -1;
+            } else {
+                double radius = area() / semiPerimeter();
+                return radius;
+            }
+        }
+
+        public double circumsCircleRadius() {
+            if (!isValid.equals("false")) {
+                return -1;
+            } else {
+                double radius = (side1 * side2 * side3) / (4 * area());
+                return radius;
+
+        }
     }
 
 }
