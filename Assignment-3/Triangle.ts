9e0d0078dd4f9a01f62c7ddbd142a17e18f96289
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-04-22 16:30:54 -0400

    fixing some stuff

--- Assignment-3/Triangle.ts
+++ Assignment-3/Triangle.ts
@@ -28,71 +28,83 @@ public isValid(): boolean {
     }
 }
 
-    // semiPerimeter method
-    public semiPerimeter (): number {
-        let semiPerimeter = (this.side1 + this.side2 + this.side3) / 2
-        return semiPerimeter
-    }
-    // area method
-    public area (): number {
-        let semiPerimeter = this.semiPerimeter()
-        let area = Math.sqrt(semiPerimeter * (semiPerimeter - this.side1) * (semiPerimeter - this.side2) * (semiPerimeter - this.side3))
-        return area
-    }
-    // get type method
-    public getType (): string {
-        if (this.side1 == this.side2 && this.side2 == this.side3) {
-            return 'Equilateral'
-        } else if (this.side1 == this.side2 || this.side2 == this.side3 || this.side3 == this.side1) {
-            return 'Isosceles'
-        } else if (Math.sqrt(this.side1) + Math.sqrt(this.side2) == Math.sqrt(this.side3) || Math.sqrt(this.side2) + Math.sqrt(this.side3) == Math.sqrt(this.side1) || Math.sqrt(this.side3) + Math.sqrt(this.side1) == Math.sqrt(this.side2)) {
-            return 'Right Angle'
-        } else {
-            return 'Scalene'
-        }
-    }
-
-    // angle method in rads
-    public angle (angleNumber: number) {
-        if (angleNumber == 1) {
-            let angle = Math.acos((Math.pow(this.side2, 2) + Math.pow(this.side3, 2) - Math.pow(this.side1, 2)) / (2 * this.side2 * this.side3))
-            return angle
-        } else if (angleNumber == 2) {
-            let angle = Math.acos((Math.pow(this.side1, 2) + Math.pow(this.side3, 2) - Math.pow(this.side2, 2)) / (2 * this.side1 * this.side3))
-            return angle
-        } else if (angleNumber == 3) {
-            let angle = Math.acos((Math.pow(this.side1, 2) + Math.pow(this.side2, 2) - Math.pow(this.side3, 2)) / (2 * this.side1 * this.side2))
-            return angle
-        }
+// semiPerimeter method
+public semiPerimeter(): number {
+    if (!this.isValid()) {
+        return -1;
     }
     
-    // get height method
-    public height (sideNumber: number): number {
-        if (sideNumber == 1) {
-            let height = (2 * this.area()) / this.side1
-            return height
-        } else if (sideNumber == 2) {
-            let height = (2 * this.area()) / this.side2
-            return height
-        } else if (sideNumber == 3) {
-            let height = (2 * this.area()) / this.side3
-            return height
-        }
-    }
+    let semiPerimeter = (this.side1 + this.side2 + this.side3) / 2;
+    return semiPerimeter;
+}
 
-    // inner circle radius method
-    public innerCircleRadius (): number {
-        let area = this.area()
-        let semiPerimeter = this.semiPerimeter()
-        let innerCircleRadius = area / semiPerimeter
-        return innerCircleRadius
+  // area method
+public area(): number {
+    if (!this.isValid()) {
+        return -1;
     }
+    
+    let semiPerimeter = this.semiPerimeter();
+    let area = Math.sqrt(semiPerimeter * (semiPerimeter - this.side1) * (semiPerimeter - this.side2) * (semiPerimeter - this.side3));
+    return area;
+}
 
-   // circumcircle circumference method
-    public circumsicleRadius() : number {
-        let circumsicleRadius = (this.side1 * this.side2 * this.side3) / (4 * this.area())
-        return circumsicleRadius
+// angle method in rads
+public angle(angleNumber: number): number {
+    if (!this.isValid()) {
+        return -1;
     }
+    
+    if (angleNumber == 1) {
+        let angle = Math.acos((Math.pow(this.side2, 2) + Math.pow(this.side3, 2) - Math.pow(this.side1, 2)) / (2 * this.side2 * this.side3));
+        return angle;
+    } else if (angleNumber == 2) {
+        let angle = Math.acos((Math.pow(this.side1, 2) + Math.pow(this.side3, 2) - Math.pow(this.side2, 2)) / (2 * this.side1 * this.side3));
+        return angle;
+    } else if (angleNumber == 3) {
+        let angle = Math.acos((Math.pow(this.side1, 2) + Math.pow(this.side2, 2) - Math.pow(this.side3, 2)) / (2 * this.side1 * this.side2));
+        return angle;
+    }
+}
+
+// height method
+public height(sideNumber: number): number {
+    if (!this.isValid()) {
+        return -1;
+    }
+    
+    if (sideNumber == 1) {
+        let height = (2 * this.area()) / this.side1;
+        return height;
+    } else if (sideNumber == 2) {
+        let height = (2 * this.area()) / this.side2;
+        return height;
+    } else if (sideNumber == 3) {
+        let height = (2 * this.area()) / this.side3;
+        return height;
+    }
+}
+
+// inner circle radius method
+public innerCircleRadius(): number {
+    if (!this.isValid()) {
+        return -1;
+    }
+    
+    let area = this.area();
+    let semiPerimeter = this.semiPerimeter();
+    let innerCircleRadius = area / semiPerimeter;
+    return innerCircleRadius;
+}
+
+// circumsCircleRadius method
+public circumsCircleRadius(): number {
+    if (!this.isValid()) {
+        return -1;
+    }
+    
+    let circumsCircleRadius = (this.side1 * this.side2 * this.side3) / (4 * this.area());
+    return circumsCircleRadius;
 }
 
 export default Triangle
