13405f4f1703c9bf0e1400f3ffdb78f07d60331d
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-04-23 21:56:14 -0400

    java still not working, but getting closer

--- Assignment-3/Main.java
+++ Assignment-3/Main.java
@@ -17,17 +17,20 @@ final class Main {
         Triangle triangle1 = new Triangle(3, 4, 5);
         System.out.println("Created a 3 mm by 4 mm by 5 mm triangle.");
         System.out.println("Is the triangle valid? " + triangle1.isValid());
-        System.out.println("Semiperimeter " + triangle1.semiPerimeter() + " mm.");
-        System.out.println("Area " + triangle1.area() + " mm^2.");
+        System.out.println("Semiperimeter: %.4f " + triangle1.semiPerimeter() + " mm.");
+        System.out.println("Area: %.4f " + triangle1.area() + " mm^2.");
         System.out.println("Type " + triangle1.getType());
-        System.out.println("Angle 1 " + triangle1.angle(1) + " rads.");
-        System.out.println("Angle 2 " + triangle1.angle(2) + " rads.");
-        System.out.println("Angle 3 " + triangle1.angle(3) + " rads.");
-        System.out.println("Height 1: " + triangle1.height(1) + " mm.");
-        System.out.println("Height 2: " + triangle1.height(2) + " mm.");
-        System.out.println("Height 3: " + triangle1.height(3) + " mm.");
-        System.out.println("Inner circle radius: " + triangle1.innerCircleRadius() + " mm.");
-        System.out.println("Circumcircle radius: " + triangle1.circumsCircleRadius() + " mm.");
+        System.out.println("Angle 1: %.4f " + triangle1.angle(1) + " rads.");
+        System.out.println("Angle 2: %.4f " + triangle1.angle(2) + " rads.");
+        System.out.println("Angle 3: %.4f " + triangle1.angle(3) + " rads.");
+        System.out.println("Height 1: %.4f " + triangle1.height(1) + " mm.");
+        System.out.println("Height 2: %.4f " + triangle1.height(2) + " mm.");
+        System.out.println("Height 3: %.4f " + triangle1.height(3) + " mm.");
+        System.out.println("Inner circle radius: %.4f " + triangle1.innerCircleRadius() + " mm.");
+        System.out.println("Circumcircle radius: %.4f " + triangle1.circumsCircleRadius() + " mm.");
+        System.out.println("\n");
+
+        System.out.println("\nDone.");
 
     }
 }
