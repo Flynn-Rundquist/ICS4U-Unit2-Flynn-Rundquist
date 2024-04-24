3b0d2860f8e8517362690aaba433a221f50b849b
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-04-23 21:24:51 -0400

    Update Triangle.java

--- Assignment-3/Triangle.java
+++ Assignment-3/Triangle.java
@@ -19,7 +19,7 @@ public class Triangle {
     }
 
     public String isValid() {
-        if (side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side1) {
+        if (side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side1) {
             return "true";
         } else {
             return "false";
@@ -27,17 +27,27 @@ public class Triangle {
     }
 
     public double semiPerimeter() {
-        int semiPerimeter = (side1 + side2 + side3) / 2;
-        return semiPerimeter;
+        if (!isValid().equals("false")) {
+            return -1;
+        } else {
+            int semiPerimeter = (side1 + side2 + side3) / 2;
+            return semiPerimeter;
+        }
     }
 
     public double area() {
-        double area = Math.sqrt(semiPerimeter() * (semiPerimeter() - side1) * (semiPerimeter() - side2) * (semiPerimeter() - side3));
-        return area;
+        if (!isValid().equals("false")) {
+            return -1;
+        } else {
+            double area = Math.sqrt(semiPerimeter() * (semiPerimeter() - side1) * (semiPerimeter() - side2) * (semiPerimeter() - side3));
+            return area;
+        }
     }
 
     public String getType() {
-        if (side1 == side2 && side2 == side3) {
+        if (!isValid().equals("false")) {
+            return "-1";
+        } else if (side1 == side2 && side2 == side3) {
             return "Equilateral";
         } else if (side1 == side2 || side2 == side3 || side3 == side1) {
             return "Isosceles";
@@ -46,8 +56,11 @@ public class Triangle {
         }
     }
 
-    public double angle(double angleNumber) {
-        if (angleNumber == 1) {
+    public double angle(int angleNumber) {
+        
+        if (!isValid().equals("false")) {
+            return -1;
+        } else if (angleNumber == 1) {
             double angle = Math.acos((Math.pow(side2, 2) + Math.pow(side3, 2) - Math.pow(side1, 2)) / (2 * side2 * side3));
             return angle;
         } else if (angleNumber == 2) {
@@ -59,8 +72,10 @@ public class Triangle {
         }
     }
 
-    public double height(double sideNumber) {
-        if (sideNumber == 1) {
+    public double height(int sideNumber) {
+        if (!isValid().equals("false")) {
+            return -1;
+        } else if (sideNumber == 1) {
             double height = (2 * area() / side1);
             return height;
         } else if (sideNumber == 2) {
@@ -73,12 +88,22 @@ public class Triangle {
         }
 
     public double innerCircleRadius() {
-        double radius = area() / semiPerimeter();
-        return radius;
+        if (!isValid().equals("false")) {
+            return -1;
+        } else {
+            double radius = area() / semiPerimeter();
+            return radius;
+        }
     }
 
     public double circumsCircleRadius() {
-        double radius = (side1 * side2 * side3) / (4 * area());
-        return radius;
+        if (!isValid().equals("false")) {
+            return -1;
+        } else {
+            double radius = (side1 * side2 * side3) / (4 * area());
+            return radius;
+
         }
     }
+
+}
