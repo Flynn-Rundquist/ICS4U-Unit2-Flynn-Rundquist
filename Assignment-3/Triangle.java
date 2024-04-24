296f0652624714f9f855f633e1d9d4728e186fc4
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-04-24 09:26:20 -0400

    linting

--- Assignment-3/Triangle.java
+++ Assignment-3/Triangle.java
@@ -6,26 +6,51 @@
  * @since 2024-04-22
  */
 
-
-public class Triangle {
+class Triangle {
+    /**
+     * The main() method.
+     *
+     * @param args The command line arguments
+     */
     private int side1;
     private int side2;
     private int side3;
-    
-    public Triangle(int side1, int side2, int side3) {
+    private int numberOne = 1;
+    private int numberTwo = 2;
+    private int numberThree = 3;
+    /**
+     * The constructor.
+     *
+     * @param side1 The first side of the triangle
+     * @param side2 The second side of the triangle
+     * @param side3 The third side of the triangle
+     */
+    Triangle(int side1, int side2, int side3) {
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
     }
 
+    /**
+     * The validity of the triangle.
+     *
+     * @return isValid
+     */
     public boolean isValid() {
-        if (side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side1) {
+        if (side1 + side2 > side3
+            && side2 + side3 > side1
+            && side3 + side1 > side1) {
             return true;
         } else {
             return false;
         }
     }
 
+    /**
+     * The semiperimeter of the triangle.
+     *
+     * @return semiPerimeter
+     */
     public double semiPerimeter() {
         if (!this.isValid()) {
             return -1;
@@ -35,15 +60,29 @@ public class Triangle {
         }
     }
 
+    /**
+     * The area of the triangle.
+     *
+     * @return area
+     */
     public double area() {
         if (!this.isValid()) {
             return -1;
         } else {
-            final double area = Math.sqrt(semiPerimeter() * (semiPerimeter() - side1) * (semiPerimeter() - side2) * (semiPerimeter() - side3));
+            final double area =
+                Math.sqrt(semiPerimeter()
+                * (semiPerimeter() - side1)
+                * (semiPerimeter() - side2)
+                * (semiPerimeter() - side3));
             return area;
         }
     }
 
+    /**
+     * The type of the triangle.
+     *
+     * @return type
+     */
     public String getType() {
         if (!this.isValid()) {
             return "-1";
@@ -56,38 +95,61 @@ public class Triangle {
         }
     }
 
+    /**
+     * The angle of the triangle.
+     *
+     * @param angleNumber The angle number
+     * @return angle
+     */
     public double angle(double angleNumber) {
         if (!this.isValid()) {
             return -1;
-        } else if (angleNumber == 1) {
-            final double angle = Math.acos((Math.pow(side2, 2) + Math.pow(side3, 2) - Math.pow(side1, 2)) / (2 * side2 * side3));
+        } else if (angleNumber == numberOne) {
+            final double angle = Math.acos((Math.pow(side2, 2)
+                + Math.pow(side3, 2)
+                - Math.pow(side1, 2)) / (2 * side2 * side3));
             return angle;
-        } else if (angleNumber == 2) {
-            final double angle = Math.acos((Math.pow(side3, 2) + Math.pow(side1, 2) - Math.pow(side2, 2)) / (2 * side3 * side1));
+        } else if (angleNumber == numberTwo) {
+            final double angle = Math.acos((Math.pow(side3, 2)
+                + Math.pow(side1, 2)
+                - Math.pow(side2, 2)) / (2 * side3 * side1));
             return angle;
-        } else if (angleNumber == 3) {
-            final double angle = Math.acos((Math.pow(side1, 2) + Math.pow(side2, 2) - Math.pow(side3, 2)) / (2 * side1 * side2));
+        } else if (angleNumber == numberThree) {
+            final double angle = Math.acos((Math.pow(side1, 2)
+                + Math.pow(side2, 2)
+                - Math.pow(side3, 2)) / (2 * side1 * side2));
             return angle;
         }
         return angle(angleNumber);
     }
 
+    /**
+     * The height of the triangle.
+     *
+     * @param sideNumber The side number
+     * @return height
+     */
     public double height(double sideNumber) {
         if (!this.isValid()) {
             return -1;
-        } else if (sideNumber == 1) {
-            final double height = (2 * area() / side1);
+        } else if (sideNumber == numberOne) {
+            final double height = 2 * area() / side1;
             return height;
-        } else if (sideNumber == 2) {
-            final double height = (2 * area() / side2);
+        } else if (sideNumber == numberTwo) {
+            final double height = 2 * area() / side2;
             return height;
-        } else if (sideNumber == 3) {
-            final double height = (2 * area() / side3);
+        } else if (sideNumber == numberThree) {
+            final double height = 2 * area() / side3;
             return height;
         }
         return height(sideNumber);
-        }
+    }
 
+    /**
+     * The inner circle radius.
+     *
+     * @return radius
+     */
     public double innerCircleRadius() {
         if (!this.isValid()) {
             return -1;
@@ -95,8 +157,14 @@ public class Triangle {
             final double radius = area() / semiPerimeter();
             return radius;
         }
+
     }
 
+    /**
+     * The circumcircle radius.
+     *
+     * @return radius
+     */
     public double circumsCircleRadius() {
         if (!this.isValid()) {
             return -1;
